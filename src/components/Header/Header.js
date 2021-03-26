53e8e327ebb02dbb4d63eac9053e5ae6b4ae27ff
Peaches_MLG <PeachesMLG100@gmail.com>
2021-03-26 16:52:23 +0000

    CSS Changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,11 +5,10 @@ export default function Header(props) {
     const {logo} = props;
     return (
         <div className="Header">
-            <div className="Header-Logo">
+            <a href="/" className="Header-Logo">
                 <img alt="Logo" src={logo}/>
-                <h1>IB Electrical
-                </h1>
-            </div>
+                <h1>IB Electrical</h1>
+            </a>
             <div className="Header-Content">
                 <a href="/">Home</a>
                 <a href="/about">About Us</a>
