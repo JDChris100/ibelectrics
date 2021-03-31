080301c605b5d6fd9dd2da1cf6023ed4ab7a9409
Peaches_MLG <PeachesMLG100@gmail.com>
2021-03-31 12:23:25 +0100

    Changes

--- src/index.js
+++ src/index.js
@@ -2,14 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-import logo from './graphics/logo.svg';
-
 import Header from "./components/Header/Header";
 
 ReactDOM.render(
     <React.StrictMode>
-        <Header logo={logo}/>
-        <p>Hello World!</p>
+        <Header/>
     </React.StrictMode>,
     document.getElementById('root')
 );
